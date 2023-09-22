f32cbb22dd24c34e830e288d8f9766c45c2eea21
SushiV <xjanedoee123@gmail.com>
2023-09-22 14:33:57 -0400

    Testing this push

--- main.tf
+++ main.tf
@@ -4,4 +4,7 @@ Adding this change to pull changes from github to gitpod tutoral
 Push this to a new commit
 updating this to github to see changes practice
 
-Pushing this again to test out this
+Pushing this again to test out this+
+Pushing from vs code 
+
+Pushing to git hub
