95bc730fd8f535717a7ccd517900c1067a7e4797
SushiV <xjanedoee123@gmail.com>
2023-09-22 14:37:05 -0400

    Deleted from testing

--- main.tf
+++ main.tf
@@ -1,12 +1,2 @@
 
 # 1st change tutorial
-Adding this change to pull changes from github to gitpod tutoral
-Push this to a new commit
-updating this to github to see changes practice
-
-Pushing this again to test out this+
-Pushing from vs code 
-
-Pushing to git hub
-
-Editing from github to test if itll show on VS code
