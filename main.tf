3bd995b431af48f6a40629255da6719a8cc6c262
SushiV <111544292+SushiSV@users.noreply.github.com>
2023-10-06 16:27:29 -0400

    Update main.tf
    removed random strings cause it will no longer be used

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-resource "random_string" "bucket_name" {
-  lower = true
-  upper = false
-  length   = 32
-  special  = false
-}
-
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "example" {
   # Bucket Naming Rules
