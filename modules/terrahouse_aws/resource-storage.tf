cbfbaa7726a9b6be3d23301d17197c9135e3cef1
Sushi Vang <xjanedoee123@gmail.com>
2023-10-12 20:58:39 +0000

    update-3

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -11,17 +11,18 @@ resource "aws_s3_bucket" "website_bucket" {
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
-#resource "aws_s3_bucket_website_configuration" "website_configuration" {
-#  bucket = aws_s3_bucket.website_bucket.bucket
-#
-#  index_document {
-##    suffix = "index.html"
-#  }
-#
-#  error_document {
-#    key = "error.html"
-#  }
-#}
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+
+  index_document {
+#    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
 ## https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
 #resource "aws_s3_object" "index_html" {
 #  bucket = aws_s3_bucket.website_bucket.bucket
